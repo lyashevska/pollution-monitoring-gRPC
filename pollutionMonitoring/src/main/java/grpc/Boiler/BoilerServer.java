945ebf7f1a9eb401b2c4d9e0d642ed3def979c31
Olga Lyashevska <olga@herenstraat.nl>
2021-12-09 10:06:51 +0000

    jmdns added to services ventilation and boiler

--- pollutionMonitoring/src/main/java/grpc/Boiler/BoilerServer.java
+++ pollutionMonitoring/src/main/java/grpc/Boiler/BoilerServer.java
@@ -5,19 +5,29 @@ import io.grpc.ServerBuilder;
 
 import java.io.IOException;
 
+import grpc.medicalWaste.SimpleServiceRegistration;
+
 /**
  * @author patrick
- * 
+ * jmdns registration
  */
 public class BoilerServer {
 
     public static void main(String[] args) throws IOException, InterruptedException {
+    	
+		int port = 50054;
+		String service_type = "_boiler._tcp.local.";
+		String service_name = "GrpcServer";
+		SimpleServiceRegistration ssr = new SimpleServiceRegistration();
+		ssr.run(port, service_type, service_name);
+    	    	
+    	
         Server server = ServerBuilder
-                .forPort(4141)
+                .forPort(port)
                 .addService(new BoilerServiceImpl()).build();
 
         server.start();
-        System.out.println("Server created for Boiler Service: Listening on port 4141");
+        System.out.println("Server created for Boiler Service: Listening on port: " + port);
         server.awaitTermination();
     }
 }
